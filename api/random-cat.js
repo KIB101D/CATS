b12a707c73ad972f29bd8c39588674542a1f65a3
KIB101D <152413382+KIB101D@users.noreply.github.com>
2026-01-22 11:15:24 +0100

    Fix response handling in fetchTheCat function
    - grab random images from API

--- api/random-cat.js
+++ api/random-cat.js
@@ -1,12 +1,23 @@
 export async function fetchTheCat() {
   const randomSkip = Math.floor(Math.random() * 500);
-  const responce = await fetch(
-    `https://cataas.com/api/cats?limit=15&skip=${randomSkip}&height=300&width=500`
-  ); // fetch 15 cats
-  const data = await responce.json();
 
-  if (!responce.ok) {
-    throw new Error("Problem conection");
+  const response = await fetch(
+    `https://cataas.com/api/cats?limit=15&skip=${randomSkip}`
+  );
+
+  if (!response.ok) {
+    throw new Error("Problem connection");
   }
-  return data;
+
+  const data = await response.json();
+
+  return data.map((cat) => {
+    const catId = cat.id;
+
+    return {
+      ...cat,
+      id: catId,
+      imageUrl: `https://cataas.com/cat/${catId}?width=500&height=300&fit=cover&type=square`,
+    };
+  });
 }
